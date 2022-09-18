e9c72f1b39012c1f6c68abbb8c316472e171c84c
sano11o1 <mashio.tech@gmail.com>
2022-09-18 19:16:05 +0900

    vpcを追加
    cdk v1とv2でライブラリのインポート方法が異なる
    
    ドキュメントはv1
    
    2022年9月の段階で`cdk init`するとv2のテンプレートでインストールされる
    
    v1とv2の違いはこちらを参照
    https://engineering.dena.com/blog/2022/02/aws-cdk-v2/

--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -1,16 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Vpc, SubnetType } from 'aws-cdk-lib/aws-ec2';
 
 export class CdkDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const vpc = new Vpc(this, "MainVpc", {
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'public-subnet',
+          subnetType: SubnetType.PUBLIC
+        }
+      ]
+    })
   }
 }
